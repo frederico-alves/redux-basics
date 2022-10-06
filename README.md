2037af2885440eaebb5c924f20640e6c912074d8
Frederico Alves <fredericoalves.info@gmail.com>
2022-10-07 00:39:40 +0200

    redux boilerplate

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Redux Basics
 
-Based on the YouTube tutorial made by @developedbyed [@developedbyed](https://www.youtube.com/watch?v=CVpUuw9XSjY&ab_channel=developedbyed)
+Based on the YouTube tutorial made by [@developedbyed](https://www.youtube.com/watch?v=CVpUuw9XSjY&ab_channel=developedbyed)
 
 # Commands
 
 - npx create-react-app .
 - npm install redux react-redux
+
+# Steps of implementation
+`index.js`:
+
+import { createStore } from 'redux';
--- src/index copy.js
+++ src/index copy.js
@@ -5,23 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // ================= REDUX =================
 import { createStore } from 'redux';
-import allReducers from './reducers';
-import { Provider } from 'react-redux';
-
-// $ Allows us to use Redux DevTools Extension
-const myStore = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // $ Provider is a Redux component that wraps around the App component
-  <Provider store={myStore}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // ================= REDUX =================
@@ -29,16 +18,40 @@ root.render(
 // ***** STORE *****  -> GLOBALIZED STATE
 
 // ***** ACTION ***** -> DESCRIBE WHAT YOU WANT TO DO
+// It is a function that returns an object
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
 
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
 
 // ***** REDUCER *****  -> DESCRIBE HOW YOUR ACTION TRANSFORMS THE STATE INTO THE NEXT STATE
+// It is a function that returns a new state
 
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
 
 // ***** SUBSCRIBE *****  -> LISTEN TO CHANGES IN THE STATE
-
+store.subscribe(() => console.log(store.getState()));
 
 // ***** DISPATCH *****  -> EXECUTE THE CODE BASED ON THE ACTION
-
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 
 
